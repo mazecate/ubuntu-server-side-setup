1b63bea7a3e709f41e645dd651cb3c82745f4e81
Black Cat <101245572+mazecate@users.noreply.github.com>
2023-10-19 15:12:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,8 +16,10 @@ echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Source the current shell
+# Reference: https://unix.stackexchange.com/questions/184508/nvm-command-not-available-in-bash-script
 echo "Sourcing the current shell..."
 #source ~/.bashrc
+. ~/.nvm/nvm.sh
 . ~/.bashrc
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
