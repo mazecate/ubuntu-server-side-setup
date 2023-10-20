d90cada7960146db05ab616482659cac5c080872
Black Cat <101245572+mazecate@users.noreply.github.com>
2023-10-20 12:01:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,21 +30,21 @@ echo "Installing Node.js version 16.20.1..."
 nvm install 16.20.1
 
 # Install Robo3t
-echo "Installing Robo3T..."
-wget https://github.com/Studio3T/robomongo/releases/download/v1.4.4/robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz
-tar -xvzf robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz
+# echo "Installing Robo3T..."
+# wget https://github.com/Studio3T/robomongo/releases/download/v1.4.4/robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz
+# tar -xvzf robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz
 
 # old method
-sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/bin/robo3t
-sudo ln -sf /usr/local/bin/robo3t/bin/robo3t /usr/local/bin/robo3t
+# sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/bin/robo3t
+# sudo ln -sf /usr/local/bin/robo3t/bin/robo3t /usr/local/bin/robo3t
 
 # New method
 # sudo mkdir /usr/local/bin/old_robo3t
 # sudo mv /usr/local/bin/robo3t /usr/local/bin/old_robo3t
 # # sudo mkdir /usr/local/bin/robo3t
-# sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/bin/robo3t
+# sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/robo3t
 # sudo chmod +x /usr/local/bin/robo3t/bin/robo3t
-# sudo ln -sf /usr/local/bin/robo3t/bin/robo3t /usr/local/bin/robo3t
+# sudo ln -sf /usr/local/robo3t/bin/robo3t /usr/local/bin/robo3t
 
 # Open new terminal
 gnome-terminal
