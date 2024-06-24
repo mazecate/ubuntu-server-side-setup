6993856cdf732cbae27e7129cf3ead2ca6eebe99
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-06-24 17:04:48 +0800

    Create setup_mongosh.sh

---
+++ setup_mongosh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install gnupg and its required libraries using the following command
+sudo apt-get install gnupg
+
+# Import the public key used by the package management system
+wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
+
+# Create a list file for MongoDB
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# Reload local package database
+sudo apt-get update
+
+# Install the latest stable version of mongosh with the included OpenSSL libraries:
+sudo apt-get install -y mongodb-mongosh
+
+# Install mongosh with your OpenSSL 1.1 libraries:
+# sudo apt-get install -y mongodb-mongosh-shared-openssl11
+
+# Install mongosh with your OpenSSL 3.0 libraries:
+# sudo apt-get install -y mongodb-mongosh-shared-openssl3
+
+# Check version
+mongosh --version
