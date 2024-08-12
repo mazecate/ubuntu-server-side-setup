ca8fa5a4de83a9df65f842b3dffa09189c3d4a2e
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-08-12 22:09:23 +0800

    Update setup_robo3t.sh

--- setup_robo3t.sh
+++ setup_robo3t.sh
@@ -13,16 +13,9 @@ wget https://github.com/Studio3T/robomongo/releases/download/v1.4.4/robo3t-1.4.4
 tar -xvzf robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz
 sudo chmod +x robo3t-1.4.4-linux-x86_64-e6ac9ec/bin/robo3t
 
-# New method
-# sudo mkdir /usr/local/bin/old_robo3t
-# sudo mv /usr/local/bin/robo3t /usr/local/bin/old_robo3t
-# # sudo mkdir /usr/local/bin/robo3t
-# sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/robo3t
-# sudo chmod +x /usr/local/bin/robo3t/bin/robo3t
-# sudo ln -sf /usr/local/robo3t/bin/robo3t /usr/local/bin/robo3t
-
 sudo mkdir -p /usr/local/robo3t
-sudo mv -f robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/robo3t
+# sudo mv -f robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/robo3t
+sudo cp -rf robo3t-1.4.4-linux-x86_64-e6ac9ec/* /usr/local/robo3t
 sudo chmod +x /usr/local/bin/robo3t/bin/robo3t
 sudo ln -sf /usr/local/robo3t/bin/robo3t /usr/local/bin/robo3t
 
