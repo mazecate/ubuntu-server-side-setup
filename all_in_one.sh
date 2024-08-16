17327975a1bd97151898c340225e97842727fa73
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-08-16 17:41:05 +0800

    Update all_in_one.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -18,13 +18,7 @@ install_nvm() {
   
   # Source the current shell
   # Reference: https://unix.stackexchange.com/questions/184508/nvm-command-not-available-in-bash-script
-  # echo "Sourcing the current shell..."
-  #source ~/.bashrc
   . ~/.nvm/nvm.sh
-  # . ~/.bashrc
-  # export NVM_DIR="$HOME/.nvm"
-  # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   
   # Install Node.js version 16.20.1
   echo "Installing Node.js LTS version..."
