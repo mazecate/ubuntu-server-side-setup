70671974b95deb69f6b2d2864af1d58e64fde7c9
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-09-09 20:06:01 +0800

    Update install_mongodb_4_2.sh

--- install_mongodb_4_2.sh
+++ install_mongodb_4_2.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-install_mongodb() {
+install_mongodb_4_2() {
     # 更新包列表
     sudo apt-get update
 
     # 安裝必要的包
-    sudo apt-get install -y gnupg curl
+    sudo apt-get install -y gnupg
 
     # 添加 MongoDB 的 GPG 密鑰
-    curl -fsSL https://pgp.mongodb.com/server-4.2.asc | \
-       sudo gpg -o /usr/share/keyrings/mongodb-server-4.2.gpg \
-       --dearmor
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
     # 創建 MongoDB 的源列表文件
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-    #echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
     # 更新包列表
     sudo apt-get update
 
-    # 安裝 MongoDB stable software pacakge
+    # 安裝 MongoDB 4.2
     sudo apt-get install -y mongodb-org
 
     # 啟動 MongoDB 服務
@@ -28,8 +25,8 @@ install_mongodb() {
     # 設置 MongoDB 服務開機自啟
     sudo systemctl enable mongod
 
-    echo "MongoDB 安裝完成並已啟動。"
+    echo "MongoDB 4.2 安裝完成並已啟動。"
 }
 
 # 執行安裝函數
-install_mongodb
+install_mongodb_4_2
