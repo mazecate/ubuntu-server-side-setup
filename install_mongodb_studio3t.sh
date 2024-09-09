53dae715e58b4249bcb091fb947ccd023f5c6dab
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-09-09 22:13:38 +0800

    Update and rename install_mongodb.sh to install_mongodb_studio3t.sh

--- install_mongodb_studio3t.sh
+++ install_mongodb_studio3t.sh
@@ -32,5 +32,25 @@ install_mongodb() {
     echo "MongoDB 安裝完成並已啟動。"
 }
 
+# Function to install Studio 3T
+install_studio3t() {
+    # Download Studio 3T
+    curl -L -o studio-3t-linux-x64.tar.gz https://download.studio3t.com/studio-3t/linux/2024.3.1/studio-3t-linux-x64.tar.gz
+
+    # Extract the tar file
+    tar -xzf studio-3t-linux-x64.tar.gz
+
+    # Run the Studio 3T installer
+    cd studio-3t-linux-x64
+    sudo ./studio-3t-linux-x64.sh
+
+    # Clean up
+    cd ..
+    rm -rf studio-3t-linux-x64.tar.gz studio-3t-linux-x64
+
+    echo "Studio 3T installation completed!"
+}
+
 # 執行安裝函數
 install_mongodb
+install_studio3t
