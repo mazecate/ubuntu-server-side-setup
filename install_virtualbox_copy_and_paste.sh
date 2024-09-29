260a8b8909a6ca3a551c9cb93ad1022d3fc8d5db
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2024-09-29 12:54:28 +0800

    Update install_virtualbox_copy_and_paste.sh

--- install_virtualbox_copy_and_paste.sh
+++ install_virtualbox_copy_and_paste.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Reference 
+# https://medium.com/%E8%8A%B1%E5%93%A5%E7%9A%84%E5%A5%87%E5%B9%BB%E6%97%85%E7%A8%8B/%E8%A7%A3%E6%B1%BAvirtualbox%E7%84%A1%E6%B3%95%E9%9B%99%E5%90%91%E8%A4%87%E8%A3%BD%E8%B2%BC%E4%B8%8A-1554d5a81da0
+
 sudo apt install virtualbox-guest-x11
 sudo VBoxClient --clipboard
 
