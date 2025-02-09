2d4934a437776f55bd5f2351502aaf2f9ca83450
Hyper Muteki <101245572+mazecate@users.noreply.github.com>
2025-02-09 18:19:36 +0800

    Create backup_server_setting.sh

---
+++ backup_server_setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the destination directory
+DEST_DIR='path'
+
+cp --parents <file_path>
+
+# backup entire folder
+# cp --parents -r /ssl
+# cp --parents -r /ssl/*.*
+cp --parents -r <file_path>
